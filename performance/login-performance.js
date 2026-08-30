c678adf4482dd5d9f5efa0a285efa31e033a1932
Fernando Rojas Oróstica <ext_fernando.rojas@cajalosandes.cl>
2026-08-30 01:08:36 -0400

    Simular alerta por degradacion de performance

--- performance/login-performance.js
+++ performance/login-performance.js
@@ -6,7 +6,7 @@ export const options = {
   duration: '10s',
 
   thresholds: {
-    http_req_duration: ['p(95)<1000'],
+    http_req_duration: ['p(95)<1'],
     http_req_failed: ['rate<0.05'],
   },
 };
